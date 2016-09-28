41d39fcb5fae3b4be79f764ced1464a233b6d6eb
grojas <grojas@dwsoftware.mx>
2016-09-28 16:37:23 -0500

    First project commit

---
+++ app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        //return HEROES;
+        //this.heroes = this.heroService.getHeroes();
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(resolve, 2000))//delay 2 seconds
+            .then(() => this.getHeroes());
+    }
+
+}
